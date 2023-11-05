7760fc892db4e111a4e3225fc3ad57e374470604
Arilson1 <arilsontere18@hotmail.com>
2023-11-05 19:21:36 -0300

    Integração mqtt

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,6 +1,9 @@
+import { useEffect, useState } from "react";
 import { StyleSheet, View, Text, FlatList } from "react-native";
+
 import { FontAwesome5 } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons';
+import Paho from 'paho-mqtt';
 
 import { ListItem } from "../components/ListItem";
 
@@ -25,7 +28,43 @@ const DATA = [
     },
 ];
 
+client = new Paho.Client(
+    "maqiatto.com",
+    8883,
+    "mqttx_23ba8d7e"
+);
+
 export function Home() {
+
+    const [temp, setTemp] = useState(null);
+    const [umidade, setUmidade] = useState(null);
+
+    function onMessage(message) {
+        if (message.destinationName === "arilsontere18@hotmail.com/temp") {
+            setTemp(message.payloadString);
+        }
+        if (message.destinationName === "arilsontere18@hotmail.com/umidade") {
+            setUmidade(message.payloadString);
+        }
+    }
+
+    useEffect(() => {
+        client.connect({
+            userName: 'arilsontere18@hotmail.com',
+            password: '@Ri142008',
+            onSuccess: () => {
+                console.log("Connected to MQTT Broker");
+                client.subscribe("arilsontere18@hotmail.com/temp");
+                client.subscribe("arilsontere18@hotmail.com/umidade");
+                client.onMessageArrived = onMessage;
+            },
+            onFailure: (error) => {
+                console.log("Failed to connect to MQTT Broker");
+                console.log(error)
+            }
+        });
+    }, []);
+
     return (
         <View style={styles.container}>
             <View style={styles.header}>
@@ -33,23 +72,23 @@ export function Home() {
                 <View style={{ flexDirection: 'row' }}>
                     <View style={styles.box}>
                         <FontAwesome5 name="temperature-high" size={56} color="#ff6347" />
-                        <Text style={styles.boxText}>21.5°</Text>
+                        <Text style={styles.boxText}>{temp}</Text>
                     </View>
                     <View style={styles.box}>
                         <Ionicons name="water" size={57} color="#4169e1" />
-                        <Text style={styles.boxText}>90.1</Text>
+                        <Text style={styles.boxText}>{umidade}0</Text>
                     </View>
                 </View>
             </View>
 
-            <View style={{width: '100%'}}>
+            <View style={{ width: '100%' }}>
                 <View>
                     <Text style={styles.title}>Histórico</Text>
                     <Text style={{ fontSize: 20, textAlign: 'center' }}>Ultimos 7 dias</Text>
                 </View>
                 <View>
                     <FlatList
-                        style={{marginTop: 10}}
+                        style={{ marginTop: 10 }}
                         data={DATA}
                         renderItem={({ item }) => <ListItem data={item} />}
                         keyExtractor={item => item.id}
